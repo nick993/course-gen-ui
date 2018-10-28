72d5148a81d66904114a2683d204633fdc7f1406
Subhrajyoti <sjs14031994@gmail.com>
2018-10-28 09:32:44 +0530

    Initial Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "../styles/App.css";
 import "./LinkList";
+import AppBar from "./Appbar"; //eslint-disable-line
 import LinkList from "./LinkList"; //eslint-disable-line
 
 class App extends React.Component {
 	render() {
 		return (
 			<div>
+				<AppBar />
 				<LinkList />
 			</div>
 		);
--- src/components/Link.js
+++ src/components/Link.js
@@ -1,35 +1,58 @@
 import React from "react";
-import {Well, Button} from "react-bootstrap"; //eslint-disable-line
+import { Well } from "react-bootstrap"; //eslint-disable-line
+
+import { withStyles } from "@material-ui/core/styles";
+import { Button, Card, CardContent, Typography, CardActions } from "@material-ui/core"; //eslint-disable-line
 
 const styles = {
 	card: {
-		height: "200px",
-		width: "300px",
-		margin: "10px"
-	}
+		minWidth: 275,
+		margin: 10
+	},
+	bullet: {
+		display: "inline-block",
+		margin: "0 2px",
+		transform: "scale(0.8)",
+	},
+	title: {
+		fontSize: 14,
+	},
+	pos: {
+		marginBottom: 12,
+	},
 };
 
 
 class Link extends React.Component {
 
 	render() {
+		const { classes } = this.props;
+		const link = this.props.link;
 		return (
 			<div>
-				<Well style={styles.card}>
-					<h3>{this.props.link.url}</h3>
-					<p>{this.props.link.description}</p>
-					<p>
-						<Button bsStyle="primary">Visit</Button>
-					</p>
-				</Well>
+				<Card className={classes.card}>
+					<CardContent>
+						<Typography color="textSecondary" gutterBottom>
+							{link.id}
+						</Typography>
+						<Typography variant="h5" component="h2">
+							{link.url}
+						</Typography>
+						<Typography component="p">
+							{link.description}
+						</Typography>
+					</CardContent>
+					<CardActions>
+						<Button size="small">Learn More</Button>
+					</CardActions>
+				</Card>
 			</div>
-
-
-
 
 			
 		);
-	}	   
+	}
 }
 
-export default Link;
+
+
+export default withStyles(styles)(Link);
